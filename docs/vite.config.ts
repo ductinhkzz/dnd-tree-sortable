9704f79804bde08bd4794f4849b00bf57f9b1cf6
Tinh Le Duc <tinh.leduc@nashtechglobal.com>
2024-09-21 19:07:59 +0700

    Fix build docs

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,19 +3,24 @@ import * as path from 'path';
 import react from '@vitejs/plugin-react'; // you can also use @vitejs/plugin-react-swc
 import pages from 'vite-plugin-react-pages';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    pages({
-      pagesDir: path.join(__dirname, 'pages'),
-    }),
-  ],
-  base: 'dnd-tree-sortable/docs/dist',
-  resolve: {
-    alias: {
-      'dnd-tree-sortable': path.join(__dirname, '../lib'),
-      react: 'react',
-      'react-dom': 'react-dom',
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react(),
+      pages({
+        pagesDir: path.join(__dirname, 'pages'),
+      }),
+    ],
+    base:
+      process.env.NODE_ENV === 'producton'
+        ? 'https://ductinhkzz.github.io/dnd-tree-sortable'
+        : 'dnd-tree-sortable/docs/dist',
+    resolve: {
+      alias: {
+        'dnd-tree-sortable': path.join(__dirname, '../lib'),
+        react: 'react',
+        'react-dom': 'react-dom',
+      },
     },
-  },
+  };
 });
